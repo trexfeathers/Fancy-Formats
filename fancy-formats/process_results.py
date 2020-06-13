aafa935b8beb14a364d45584045bad5dfbd5cafd
Martin Yeo <martin_yeo@new-forest.org>
2020-06-13 13:09:22 +0100

    XML template using generateDS and IOF.xsd.

---
+++ fancy-formats/process_results.py
@@ -0,0 +1,10 @@
+import xml_classes
+
+file_path = "/examples/ResultList1.xml"
+rootObject = xml_classes.parse(file_path, silence=True)
+
+for class_result in rootObject.ClassResult:
+    for person_result in class_result.PersonResult:
+        person = person_result.Person
+        person_name = person.Name
+        print(" ".join((person_name.Given, person_name.Family)))
