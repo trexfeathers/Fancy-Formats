09f29a235a5e133673a92d092d42787dcbf12a1a
Martin Yeo <martin_yeo@new-forest.org>
2020-06-21 20:48:05 +0100

    Abstract init test.

--- fancy_formats/tests/process_results.py
+++ fancy_formats/tests/process_results.py
@@ -5,6 +5,21 @@ from fancy_formats import process_results
 
 
 class TestFormat:
+    def test_abstract_init(self):
+        class FormatSubClass(process_results.Format):
+            pass
+
+        with pytest.raises(TypeError, match=r"Can't instantiate .*__init__"):
+            FormatSubClass()
+
+    def test_abstract_analyse(self):
+        class FormatSubClass(process_results.Format):
+            def __init__(self):
+                super().__init__()
+
+        with pytest.raises(TypeError, match=r"Can't instantiate .*analyse"):
+            FormatSubClass()
+
     def test_need_vars(self):
         class FormatSubClass(process_results.Format):
             def __init__(self):
@@ -13,13 +28,5 @@ class TestFormat:
             def analyse(self):
                 pass
 
-        with pytest.raises(ValueError, match=r"No input arguments.*"):
-            FormatSubClass()
-
-    def test_abstract_analyse(self):
-        class FormatSubClass(process_results.Format):
-            def __init__(self):
-                super().__init__()
-
-        with pytest.raises(TypeError, match=r"Can't instantiate.*"):
+        with pytest.raises(ValueError, match=r"No input arguments .*"):
             FormatSubClass()
