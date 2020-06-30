e3383a7cd8fcd3dd03fec8a4633997027a924671
Martin Yeo <martin_yeo@new-forest.org>
2020-06-30 08:41:16 +0100

    OddsEvens.odds_evens method.

--- fancy_formats/process_results.py
+++ fancy_formats/process_results.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from inspect import _empty as inspect_empty, signature
+from pathlib import Path
 
 from fancy_formats import xml_classes
 
@@ -25,7 +26,10 @@ class Format(ABC):
             error_msg = f"No input parameters specified in class {subclass_name}"
             raise ValueError(error_msg)
 
-        if not all((isinstance(p.annotation, type) and p.annotation != inspect_empty for p in self.parameters.values())):
+        if not all((
+                isinstance(p.annotation, type)
+                and p.annotation != inspect_empty
+                for p in self.parameters.values())):
             error_msg = f"Not all input parameters have type annotations in " \
                         f"class {subclass_name}"
             raise TypeError(error_msg)
@@ -33,7 +37,7 @@ class Format(ABC):
         super().__init__()
 
     @abstractmethod
-    def analyse(self):
+    def analyse(self, save_dir: Path):
         pass
 
 
@@ -44,14 +48,45 @@ class OddsEvens(Format):
                  penalty_per: int = 10):
         super().__init__()
 
-    def analyse(self):
+    def odds_evens(self,
+                   person_race_result: xml_classes.PersonRaceResult) -> list:
+        """
+        Evaluate a the control sequence in a :class:`xml_classes.PersonRaceResult`
+        for conformance to the odds-and-evens score format (i.e. must visit odd
+        controls and even controls each in a block, only switching between once).
+
+        :param person_race_result: :class:`xml_classes.PersonRaceResult` - the
+        result to be analysed.
+        :return: a list of control codes that did not conform - were found outside
+        the first block of their 'type' (odd or even).
+        """
+
+        def control_is_odd(control_number):
+            return bool(int(control_number) % 2)
+
+        # Extract control_sequence from known PersonRaceResult structure.
+        splits_list = person_race_result.SplitTime
+        control_sequence = [split.ControlCode for split in splits_list]
+
+        penalty_controls = []
+        # Identify starting control set.
+        odd_mode = control_is_odd(control_sequence[0])
+        has_switched = False
+
+        for control in control_sequence[1:]:
+            if odd_mode != control_is_odd(control):
+                # Have identified a switch from one control set to the
+                # other.
+                if not has_switched:
+                    # Is a valid switch since this is the first time.
+                    has_switched = True
+                    odd_mode = control_is_odd(control)
+                else:
+                    # Competitor switched earlier so this is against
+                    # the rules.
+                    penalty_controls.append(control)
+
+        return penalty_controls
+
+    def analyse(self, save_dir: Path):
         pass
-
-# rootObject = xml_classes.parse(file_path, silence=True)
-# for class_result in rootObject.ClassResult:
-#     for person_result in class_result.PersonResult:
-#         person = person_result.Person
-#         person_name = person.Name
-#         print(" ".join((person_name.Given, person_name.Family)))
-
-# a = OddsEvens()
